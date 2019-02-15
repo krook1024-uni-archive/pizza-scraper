1685aae5c0a72b8b75be1f665bace60ce6345269
krook1024 <krook1024@gmail.com>
2019-02-15 11:39:49 +0100

    New gitignore and readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# pizza-scraper
+# Pizza scraper
+
+This software takes a bunch of websites and tries to scrape
+data off of them. For now it's "hardcoded", as in it contains
+no machine-learning like stuff. This is needed for an other repo of mine.
---
+++ scrape.py
@@ -0,0 +1,17 @@
+#          Pizza scraper
+#   This software takes a bunch of websites and tries to scrape
+#   data off of them. For now it's "hardcoded", as in it contains
+#   no machine-learning like stuff. This is needed for an other repo of mine.
+
+program_name    = "pizza scraper"
+program_ver     = "0.0.1"
+
+# getGino() ~ this functions tries to parse ginopizza.hu to get pizza prices and names
+def getGino():
+    # http://www.ginopizza.hu/index.php?option=com_content&view=article&id=13:vekony-tesztas-pizza&catid=8:menu
+
+# Main
+def main():
+    print("Welcome to ", program_name, " v", program_ver, ".", sep='')
+
+main()
